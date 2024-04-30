5d61070b29bcd135cd32c105d1ea9213d2d47fbb
bissli <bissli@xyz.com>
2024-04-30 13:03:04 +0000

    migrate deprecated optparse to argparse

--- src/cmdline/cmdline.py
+++ src/cmdline/cmdline.py
@@ -1,10 +1,10 @@
-"""Commandline alternative to `click` (https://github.com/pallets/click)
+"""Wrapper around python default `argparse`
 """
+import argparse
 import importlib
 import logging
 import os
 import sys
-from optparse import OptionParser
 
 from libb import replacekey, scriptname
 from log import configure_logging
@@ -29,7 +29,7 @@ def patch_environment(opts, config):
 
 def parse_args(options, usage=None, config=None):
     parser = create_parser(options, usage)
-    opts, args = parser.parse_args()
+    opts, args = parser.parse_known_args()
     configure_logging(app=opts.logapp, setup=opts.logsetup, level=opts.loglevel)
     if opts.environment is not None:
         patch_environment(opts, config)
@@ -40,7 +40,8 @@ def parse_args(options, usage=None, config=None):
 def create_parser(options, usage=None):
     """We parse each option: short, long, help, [default, [action, [dest]]]
     """
-    parser = OptionParser(usage)
+    parser = argparse.ArgumentParser(usage)
+    # specified args
     for opt in options:
         opt = list(opt)
         shortopt, longopt, helptxt = opt.pop(0), opt.pop(0), opt.pop(0)
@@ -52,11 +53,12 @@ def create_parser(options, usage=None):
         if opt:
             dest = opt.pop(0)
         if shortopt and longopt:
-            parser.add_option(shortopt, longopt, help=helptxt, action=action, default=default, dest=dest)
+            parser.add_argument(shortopt, longopt, help=helptxt, action=action, default=default, dest=dest)
         else:
-            parser.add_option(shortopt or longopt, help=helptxt, action=action, default=default, dest=dest)
-    parser.add_option('-E', '--environment', dest='environment', default=None, help='set configuration environment')
-    parser.add_option('-A', '--logapp', dest='logapp', default=scriptname(), help='set script name')
-    parser.add_option('-S', '--logsetup', dest='logsetup', default='job', help='set logging setup type')
-    parser.add_option('-V', '--loglevel', dest='loglevel', default=None, help='set logging level')
+            parser.add_argument(shortopt or longopt, help=helptxt, action=action, default=default, dest=dest)
+    # default args
+    parser.add_argument('-E', '--environment', dest='environment', default=None, help='set configuration environment')
+    parser.add_argument('-A', '--logapp', dest='logapp', default=scriptname(), help='set script name')
+    parser.add_argument('-S', '--logsetup', dest='logsetup', default='job', help='set logging setup type')
+    parser.add_argument('-V', '--loglevel', dest='loglevel', default=None, help='set logging level')
     return parser
